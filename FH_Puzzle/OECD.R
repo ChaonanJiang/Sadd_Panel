02525fce06c53b3ccd35aae1305b36bd63d50a48
Chaonan Jiang <63007565+ChaonanJiang@users.noreply.github.com>
2020-06-09 20:10:37 +0200

    update

--- FH_Puzzle/OECD.R
+++ FH_Puzzle/OECD.R
@@ -1,3 +1,4 @@
+#Figure 8: London network for inverse distance and 7 nearest neighbours (weight matrices)
 library(rworldmap)
 theCountries <- c('AUS',
                   'AUT',
@@ -62,114 +63,25 @@ mal <- mapCountryData(new_world, nameColumnToPlot="OECD", catMethod = "categoric
 
 points(FHD$long, FHD$lat,pch=16, cex = .6)
 
+#Figure 8: London network for inverse distance and 7 nearest neighbours (weight matrices)
 
+#### London Inverse Distance network
 
-#### London
-points(FHD$long[5], FHD$lat[5],pch=16, cex = .6)
-#text(FHD$long[10], FHD$lat[10]+20,"London")
 for(j in 1:24){
-    segments(FHD$long[5], FHD$lat[5],FHD$long[j], FHD$lat[j],col="blue")
-}
-a <- which(W.a[10,]!=0)
-for(j in 1:7){
-  segments(FHD$long[10], FHD$lat[10],FHD$long[a[j]], FHD$lat[a[j]],col = "blue")
-}
-text(FHD$long[10], FHD$lat[10]+2,"London")
-
-
-plot(FHD$long, FHD$lat,pch=16, cex =1,xlab = " ", ylab = " ", main = "Inverse distance",axes=FALSE,col= "red")
-for(i in 1:24){
-  for(j in (i+1):24){
-    segments(FHD$long[i], FHD$lat[i],FHD$long[j], FHD$lat[j])
-  }
-  
-}
-
-plot(FHD$long, FHD$lat,pch=16, cex =1,xlab = " ", ylab = " ", main = "7 nearest neighbours",axes=FALSE,col= "red")
-
-W.a <- as.matrix(Wn$W)
-for(i in 1:24){
-  a <- which(W.a[i,]!=0)
-  for(j in 1:7){
-    segments(FHD$long[i], FHD$lat[i],FHD$long[a[j]], FHD$lat[a[j]])
-  }
-  
-  
+    segments(FHD$long[10], FHD$lat[10],FHD$long[j], FHD$lat[j],col="blue")
 }
 
 
-
-
-
-
-
-
-#######
-theCountries <- c(
-                  'AUT',
-                  'BEL',
-                 
-                  'CHE',
-                  'DNK',
-                  'ESP',
-                  'FIN',
-                  'FRA',
-                  'GBR',
-                  'GRC',
-                  'IRL',
-                  'ISL',
-                  'ITA',
-                  
-                  'NLD',
-                  'NOR',
-                  'NZL',
-                  'PRT',
-                  'SWE',
-                  'TUR'
-                  )
-# These are the ISO3 names of the countries you'd like to plot in red
-
-ocedDF <- data.frame(country = c(
-                                 'AUT',
-                                 'BEL',
-                                
-                                 'CHE',
-                                 'DNK',
-                                 'ESP',
-                                 'FIN',
-                                 'FRA',
-                                 'GBR',
-                                 'GRC',
-                                 'IRL',
-                                 'ISL',
-                                 'ITA',
-                                
-                                 'NLD',
-                                 'NOR',
-                                 'NZL',
-                                 'PRT',
-                                 'SWE',
-                                 'TUR'
-                                ),
-                     OECD= rep("OECD",18))
-# malDF is a data.frame with the ISO3 country names plus a variable to
-# merge to the map data
-
-malMap <- joinCountryData2Map(ocedDF, joinCode = "NAME",
-                              nameJoinColumn = "country")
-# This will join your malDF data.frame to the country map data
+#### London 7NN network
 
 mal <- mapCountryData(malMap, nameColumnToPlot="OECD", catMethod = "categorical",
                       missingCountryCol = gray(.8),addLegend=F, mapTitle = " ",
                       xlim=c(-6, 20), ylim=c(42.5, 70))
 points(FHD$long, FHD$lat,pch=16, cex = 1)
 
-for(i in c(2,3,5:14,18,19,21:23)){
-  a <- which(W.a[i,]!=0)
-  for(j in 1:7){
-    segments(FHD$long[i], FHD$lat[i],FHD$long[a[j]], FHD$lat[a[j]],col="blue")
-  }
-  
-  
+a <- which(W.a[10,]!=0)
+for(j in 1:7){
+  segments(FHD$long[10], FHD$lat[10],FHD$long[a[j]], FHD$lat[a[j]],col = "blue")
 }
+text(FHD$long[10], FHD$lat[10]+2,"London")
 
